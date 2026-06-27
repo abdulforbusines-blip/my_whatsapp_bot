8967643d5912f1098c0c05f48a0293e5e783b3b7
abdulforbusines-blip <abdulforbusines@gmail.com>
2026-06-27 13:20:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-// إنشاء عميل واتساب
 const client = new Client({
     puppeteer: {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
 
-// عرض QR في الطرفية لتسجيل الدخول
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
-    console.log('📲 امسح الكود للدخول إلى واتساب');
+    console.log('📲 امسح الكود من واتساب للدخول');
 });
 
-// عند تسجيل الدخول
 client.on('ready', () => {
     console.log('✅ البوت جاهز للعمل');
 });
 
-// مثال: الرد على أي رسالة تصل
 client.on('message', async msg => {
     console.log(`📩 رسالة من ${msg.from}: ${msg.body}`);
 
@@ -28,5 +24,4 @@ client.on('message', async msg => {
     }
 });
 
-// تشغيل العميل
 client.initialize();
